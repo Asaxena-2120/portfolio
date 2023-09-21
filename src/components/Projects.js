c43b15446f8ee8ebafb0337ac17c192e978f5ca8
Aastha Saxena <saxena.aastha91@gmail.com>
2023-09-21 14:48:53 -0400

    added one project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,9 +16,7 @@ export default function Projects() {
             Apps I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+            Check out my projects, involving front-end technologies (like HTML, CSS, JavaScript, React) and back-end technologies (like Node.js, Python Flask), databases and web-servers.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 export const projects = [
     {
-      title: "Project 1",
-      subtitle: "CodeSharing",
+      title: "CodeSnippetSharingPlatform",
+      subtitle: "HTML, CSS, JavaScript",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "https://media.giphy.com/media/u1WhXLjwgcXpHJBMRM/giphy.gif",
-      link: "https://sites.google.com/view/aastha-saxena/",
+        "The Code Snippet Sharing Platform is a web application that allows developers to share and discover code snippets.",
+      image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2gzMnJ2bW82dHFiZW9meXZocWJ5YjJsOXh4ZzZ5NzNtM2pjNzczYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JExzg781ZqioF7qew1/giphy.gif",
+      link: "https://github.com/Asaxena-2120/CodingSnippetSharingPlatform",
     },
     {
       title: "React Project",
@@ -33,5 +33,5 @@ export const projects = [
     },
   ];
 
-  export const skills=['React', 'Javascript', 'HTML', 'Python']
+  export const skills=['Front-End Development','Back-End Development','React', 'Javascript', 'HTML', 'Python', 'Flask', 'SQL', 'MySQL', 'Git','GitHub','Design Fundamentals', 'Web Architecture','CSS']
   export const AasthaImage = "./AasthaImage.png"
